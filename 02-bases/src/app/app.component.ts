85c80b4da7eeead8772982b97423b48996b94573
sdsanchezm <sdsanchezm@gmail.com>
2023-08-20 16:40:25 -0500

    first component angular

--- 02-bases/src/app/app.component.ts
+++ 02-bases/src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+// this is adecorator that transform the class into a component
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'pets page';
+  public counter: number= 10;
+
+  increaseBy( value: number): void {
+    this.counter += value;
+  }
+
+  decreaseBy(): void {
+    this.counter -= 1;
+  }
+
+  resetButton(): void{
+    this.counter = 10;
+  }
 }
